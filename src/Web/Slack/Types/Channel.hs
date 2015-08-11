97b461aeeb4dbab540feeb5870d772d09bb98946
Matthew Peddie <mpeddie@gmail.com>
2015-08-11 10:46:36 +1000

    If the 'is_general' field is missing from a 'channel' object, default to 'False'

--- src/Web/Slack/Types/Channel.hs
+++ src/Web/Slack/Types/Channel.hs
@@ -6,6 +6,7 @@ import Data.Aeson.Types
 import Control.Applicative
 import Data.Text (Text)
 import Control.Lens.TH
+import Data.Maybe (fromMaybe)
 
 import Web.Slack.Types.Id
 import Web.Slack.Types.Time
@@ -31,7 +32,7 @@ makeLenses ''Channel
 instance FromJSON Channel where
   parseJSON = withObject "Channel" (\o -> Channel <$> o .: "id" <*> o .: "name"
                                                   <*> o .: "created" <*> o .:"creator"
-                                                  <*> o .:? "is_archived" <*> o .: "is_general"
+                                                  <*> o .:? "is_archived" <*> fmap (fromMaybe False) (o .:? "is_general")
                                                   <*> o .:? "members" <*> o .:? "topic"
                                                   <*> o .:? "purpose" <*> o .: "is_member"
                                                   <*> (pure $ parseMaybe parseJSON (Object o) :: Parser (Maybe ChannelOpt)))
