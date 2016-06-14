71834a282c582ad2b9525bfd33ae203c2ad39132
Matthew Pickering <matthewtpickering@gmail.com>
2016-06-14 12:00:22 +0100

    Remove autoplayChatSounds field

--- src/Web/Slack/Types/Preferences.hs
+++ src/Web/Slack/Types/Preferences.hs
@@ -64,7 +64,6 @@ data Preferences = Preferences
                  , _prefEnterIsSpecialInTbt             :: Bool
                  , _prefGraphicEmoticons                :: Bool
                  , _prefConvertEmoticons                :: Bool
-                 , _prefAutoplayChatSounds              :: Bool
                  , _prefSsEmojis                        :: Bool
                  , _prefSidebarBehavior                 :: Text
                  , _prefMarkMsgsReadImmediately         :: Bool
