56818fdfe9fe716cab5d77e3759cb01727ad3380
Alex Sayers <alex.sayers@gmail.com>
2017-06-21 10:19:19 +0900

    Change API to be based on `SlackHandle`s
    The current API is quite opinionated about how users should structure
    their programs, whereas the SlackHandle-based API introduced by this
    patch is more flexible. See the example programs in the next commit for
    some illustrations of this flexibility.

--- src/Web/Slack.hs
+++ src/Web/Slack.hs
@@ -1,115 +1,194 @@
-{-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE LambdaCase                 #-}
-{-# LANGUAGE NoMonomorphismRestriction  #-}
-{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 
-{-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE TypeSynonymInstances       #-}
-{-# LANGUAGE CPP                        #-}
-------------------------------------------
--- |
--- This module exposes functionality to write bots which responds
--- to `Event`s sent by the RTM API. By using the user state parameter `s`
--- complicated interactions can be established.
+-- | Bindings to the various Slack APIs, for writing chat bots.
 --
--- This basic example echos every message the bot recieves.
--- Other examples can be found in the
--- @<https://github.com/mpickering/slack-api/tree/master/example example>@ directory.
---
--- > myConfig :: SlackConfig
--- > myConfig = SlackConfig
--- >         { _slackApiToken = "..." -- Specify your API token here
--- >         }
--- >
--- > -- type SlackBot s = Event -> Slack s ()
--- > echoBot :: SlackBot ()
--- > echoBot (Message cid _ msg _ _ _) = sendMessage cid msg
--- > echoBot _ = return ()
--- >
 -- > main :: IO ()
--- > main = runBot myConfig echoBot ()
+-- > main = withSlackHandle myConfig echoBot
+-- >
+-- > myConfig :: SlackConfig
+-- > myConfig = SlackConfig { _slackApiToken = "your API token here" }
+-- >
+-- > -- | For all channels of which this bot is a member, it simply watches
+-- > -- for messages and echoes them back to the channel they came from.
+-- > echoBot :: SlackHandle -> IO ()
+-- > echoBot h = forever $ do
+-- >     event <- getNextEvent h
+-- >     case event of
+-- >         Message chan _ msg _ _ _ ->
+-- >             sendMessage h chan msg
+-- >         _ -> return ()
 --
-module Web.Slack ( runBot
-                 -- Re-exports
-                 , Slack(..)
-                 , SlackBot
-                 , SlackState(..)
-                 , userState
-                 , session
-                 , module Web.Slack.Types
-                 , SlackConfig(..)
-                 ) where
-
-#if !MIN_VERSION_base(4,8,0)
-import           Control.Applicative
-#endif
-import           Control.Lens
-import           Control.Monad.Except
-import qualified Control.Monad.State        as S
-import qualified Data.ByteString.Lazy.Char8 as BC
-import qualified Data.Text                  as T
-import qualified Network.URI                as URI
-import qualified Network.WebSockets         as WS
-
-import           Data.Aeson
-
-import           Web.Slack.State
-import           Web.Slack.Types
-import           Web.Slack.WebAPI
-import           Wuss
-
--- | Run a `SlackBot`. The supplied bot will respond to all events sent by
--- the Slack RTM API.
+-- Slack exposes a number of APIs which provide different (but overlapping)
+-- functionality:
 --
--- Be warned that this function will throw an `IOError` if the connection
--- to the Slack API fails.
-runBot :: forall s . SlackConfig -> SlackBot s -> s -> IO ()
-runBot conf bot start = do
-  (url, sessionInfo) <- crashOnError $ rtm_start conf
-  let partialState :: Metainfo -> SlackState s
-      partialState metainfo = SlackState metainfo sessionInfo start conf
-  putStrLn "rtm.start call successful"
-  case parseWebSocketUrl (T.unpack url) of
-    Just (host, path) ->
-      runSecureClient host port path (mkBot partialState bot)
-    Nothing -> error $ "Couldn't parse WebSockets URL: " ++ T.unpack url
-  where
-    port = 443
-    parseWebSocketUrl :: String -> Maybe (String, String)
-    parseWebSocketUrl url = do
-      uri  <- URI.parseURI url
-      name <- URI.uriRegName <$> URI.uriAuthority uri
-      return (name, URI.uriPath uri)
-    crashOnError :: ExceptT T.Text IO a -> IO a
-    crashOnError = either (ioError . userError . T.unpack) return <=< runExceptT
+-- * The Real-Time Messaging API (<https://api.slack.com/rtm>) is
+--   a WebSocket-based API that allows you to receive events from Slack in
+--   real time and send basic messages.
+-- * The Web API (<https://api.slack.com/web>) consists of HTTP RPC-style
+--   methods. It provides support for more complex interactions with Slack,
+--   such as posting messages with buttons, uploading files, making
+--   reminders, etc.
+--
+-- This library is mostly about the RTM API. It has some very limited
+-- support for using the Web API.
+--
+-- For more documentationm, take a look at the "example" directory for some
+-- working slack bots.
+module Web.Slack
+    ( -- * Creating slack sessions
+      SlackHandle
+    , withSlackHandle
 
-mkBot :: (Metainfo -> SlackState s) -> SlackBot s -> WS.ClientApp ()
-mkBot partialState bot conn = do
-    let initMeta = Meta conn 0
-    WS.forkPingThread conn 10
-    botLoop (partialState initMeta) bot
+      -- * Getting data from slack
+    , getNextEvent
+    , getSession
+    , getConfig
 
-botLoop :: forall s . SlackState s -> SlackBot s -> IO ()
-botLoop st f =
-  () <$ (flip S.runStateT st  . runSlack $ forever loop)
-  where
-    loop :: Slack s ()
-    loop = do
-      conn <- use connection
-      raw <- liftIO $ WS.receiveData conn
-      let (msg :: Either String Event) = eitherDecode raw
-      case msg of
+      -- * Sending messages to slack
+    , sendMessage
+    , sendRichMessage
+    , sendPing
+
+      -- * Type re-exports
+    , SlackConfig(..)
+    , SlackSession(..)
+    , Event(..)
+    , module Web.Slack.Types
+    ) where
+
+import Control.Error
+import Control.Monad.Except
+import Data.Aeson
+import Data.IORef
+import Data.Monoid
+import qualified Data.Text as T
+import Data.Time.Clock.POSIX
+import qualified Network.URI as URI
+import qualified Network.WebSockets as WS
+import Web.Slack.Types
+import Web.Slack.WebAPI
+import Wuss
+
+-- | This library exposes a simple handle-based API. A `SlackHandle` is
+-- a handle to an open RTM session. It can also be used to make calls to
+-- the web API.
+data SlackHandle = SlackHandle
+    { _shConfig     :: SlackConfig
+    , _shSession    :: SlackSession
+    , _shConnection :: WS.Connection
+    , _shCounter    :: IORef Int
+    }
+
+-- | Log into slack with the provided credentials and pass the resulting
+-- session handle to the callback provided.
+withSlackHandle :: SlackConfig -> (SlackHandle -> IO a) -> IO a
+withSlackHandle conf fn = crashOnError $ do
+    (url, sessionInfo) <- rtm_start conf
+    (host, path) <- parseWebSocketUrl url
+    liftIO $ runSecureClient host 443 path $ \conn -> do
+        freshCounter <- newIORef 0
+        let h = SlackHandle
+              { _shConfig = conf
+              , _shConnection = conn
+              , _shSession = sessionInfo
+              , _shCounter = freshCounter
+              }
+        WS.forkPingThread conn 10
+        fn h
+
+parseWebSocketUrl :: Monad m => T.Text -> ExceptT T.Text m (String, String)
+parseWebSocketUrl url = do
+    uri  <- URI.parseURI (T.unpack url) ?? ("Couldn't parse WebSockets URL: " <> url)
+    auth <- URI.uriAuthority uri ?? ("No authority: " <> url)
+    return (URI.uriRegName auth, URI.uriPath uri)
+
+-- | Retrieve the config used to initiate the session.
+getConfig :: SlackHandle -> SlackConfig
+getConfig = _shConfig
+
+-- | When the connection is established, the slack server sends a bunch of
+-- session information. This is accessible here.
+--
+-- (Caveat: this information represents things as they were when the
+-- session was established; it is liable to become stale. If you care about
+-- keeping an up-to-date view of this stuff, you need to track changes to
+-- it using 'getNextEvent'.)
+getSession :: SlackHandle -> SlackSession
+getSession = _shSession
+
+-- | Get the next event in the queue. If the queue is empty, this function
+-- blocks until a new event is recieved.
+--
+-- If you want to write your bot in a streaming style, this function can be
+-- easily adapted into a `Producer`:
+--
+-- > eventProducer :: MonadIO m => SlackHandle -> Producer Event m ()
+-- > eventProducer h = forever $ liftIO (getNextEvent h) >>= yield
+getNextEvent :: SlackHandle -> IO Event
+getNextEvent h@SlackHandle{..} = do
+    raw <- WS.receiveData _shConnection
+    case eitherDecode raw of
         Left e -> do
-                    liftIO $ BC.putStrLn raw
-                    liftIO $ putStrLn e
-                    liftIO . putStrLn $ "Please report this failure to the github issue tracker"
-        Right event@(UnknownEvent e) -> do
-                    liftIO . print $ e
-                    liftIO . putStrLn $ "Failed to parse to a known event"
-                    liftIO . putStrLn $ "Please report this failure to the github issue tracker"
-                    -- Still handle the event if a user wants to
-                    f event
-        Right event -> f event
+            putStrLn $ unlines
+                [ show raw
+                , e
+                , "Please report this failure to the github issue tracker"
+                ]
+            getNextEvent h
+        Right event@(UnknownEvent val) -> do
+            putStrLn $ unlines
+                [ show val
+                , "Failed to parse to a known event"
+                , "Please report this failure to the github issue tracker"
+                ]
+            return event
+        Right event ->
+            return event
 
+nextMessageId :: SlackHandle -> IO Int
+nextMessageId SlackHandle{..} = do
+    liftIO $ modifyIORef _shCounter (+1)
+    liftIO $ readIORef _shCounter
+
+-- | Post a simple message to the specified channel. From the slack docs:
+--
+-- * Clients should limit messages sent to channels to 4000 characters.
+-- * Clients should not send more than one message per second sustained.
+sendMessage :: SlackHandle -> ChannelId -> T.Text -> IO ()
+sendMessage h@SlackHandle{..} cid message = do
+    uid <- nextMessageId h
+    let payload = MessagePayload uid "message" cid message
+    WS.sendTextData _shConnection (encode payload)
+
+-- | Send a ping to the server, which should respond by sending a 'Pong'
+-- event.
+sendPing :: SlackHandle -> IO ()
+sendPing h@SlackHandle{..} = do
+    uid <- nextMessageId h
+    now <- round <$> getPOSIXTime
+    let payload = PingPayload uid "ping" now
+    WS.sendTextData _shConnection (encode payload)
+
+-- | Post a complex message using the web API. There's a lot more
+-- functionality than is exposed here - see
+-- <https://api.slack.com/methods/chat.postMessage>.
+--
+-- Note that, since this function uses the slack web API (not the RTD api)
+-- under the hood, it behaves a bit differently to @sendMessage@. In
+-- particular: rich messages sent by your bot will appear as events. You
+-- will probably want to explicitly ignore these.
+sendRichMessage
+    :: SlackHandle -> ChannelId -> T.Text -> [Attachment] -> IO (Either T.Text ())
+sendRichMessage h cid msg as =
+    runExceptT $ chat_postMessage (getConfig h) cid msg as
+
+-------------------------------------------------------------------------------
+-- Helpers
+
+crashOnError :: MonadIO m => ExceptT T.Text m a -> m a
+crashOnError x = runExceptT x >>= \case
+    Left  e -> liftIO $ ioError $ userError (T.unpack e)
+    Right a -> return a
--- tests/Tests/ConnectionTest.hs
+++ tests/Tests/ConnectionTest.hs
@@ -1,26 +1,24 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Tests.ConnectionTest (main) where
 
+import Data.Maybe
+import System.Environment
 import Web.Slack
-import System.Environment (lookupEnv)
-import System.Exit
-import System.IO.Unsafe
-
-myConfig :: String -> SlackConfig
-myConfig apiToken = SlackConfig
-         { _slackApiToken = apiToken -- Specify your API token here
-         }
-
-connectBot :: SlackBot ()
-connectBot Hello = unsafePerformIO exitSuccess
-connectBot _ = return ()
-
 
 main :: IO ()
 main = do
-  apiToken <- lookupEnv "SLACK_API_TOKEN"
-  case apiToken of
-    Nothing -> exitFailure
-    Just token ->  runBot (myConfig token) connectBot ()
+    conf <- mkConfig
+    withSlackHandle conf inertBot
 
+mkConfig :: IO SlackConfig
+mkConfig = do
+    x <- lookupEnv "SLACK_API_TOKEN"
+    let apiToken = fromMaybe (error "SLACK_API_TOKEN not set") x
+    return SlackConfig{ _slackApiToken = apiToken }
 
-
+inertBot :: SlackHandle -> IO ()
+inertBot h =
+    getNextEvent h >>= \case
+        Hello -> return ()
+        e -> error ("Unexpected event: " ++ show e)
