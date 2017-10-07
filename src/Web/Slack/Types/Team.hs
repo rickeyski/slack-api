7b7467233e1db7546c317507289965d73554033b
Jason Shipman <jasonpshipman@gmail.com>
2017-10-07 19:39:14 -0400

    Type mismatch for team icon has been fixed by Slack HQ - revert #78

--- src/Web/Slack/Types/Team.hs
+++ src/Web/Slack/Types/Team.hs
@@ -28,7 +28,7 @@ data TeamIcons = TeamIcons
                , _teamIcon88      :: URL
                , _teamIcon102     :: URL
                , _teamIcon132     :: URL
-               , _teamIconDefault :: Maybe Text
+               , _teamIconDefault :: Maybe Bool
                } deriving Show
 
 makeLenses ''Team
